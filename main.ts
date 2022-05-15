1d749d79c7cd9fab1b26a5f3fcab3602aa96bb24
JorgeHTcoding <jht.coding@gmail.com>
2022-05-15 13:56:47 +0200

    Update1 Clase Professional y Main - Jorge

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { Professional } from "./professional";
+
+
+let professional1 = new Professional("PepitoPalote",65,"Male",71,189,"Castaño","Azul","Caucasico",true,"Irlandés",58,"Actor");
+let professional2 = new Professional("Paquita La del Barrio",75,"Female",82,159,"Rubio","Avellana","Caucasico",true,"Mexicana",0,"Cantante");
+let professional3 = new Professional("Clint Eastwood",91,"Male",80,190,"Gris","Azul","Caucasico",false,"Estado Unidense",2,"Actor, Director");
+let professional4 = new Professional("Donnie Yen",58,"Male",68,173,"Negro","Negro","Asiático",false,"Chino",0,"Actor, Director, Productor");
+
+professional1.consolePrint();
+professional2.consolePrint();
+professional3.consolePrint();
+professional4.consolePrint();
---
+++ professional.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+exports.Professional = void 0;
+var Professional = /** @class */ (function () {
+    function Professional(name, age, genre, weight, height, hairColor, eyeColor, race, isRetired, nationality, oscarsNumber, profession) {
+        this.name = name;
+        this.age = age;
+        this.genre = genre;
+        this.weight = weight;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.race = race;
+        this.isRetired = isRetired;
+        this.nationality = nationality;
+        this.oscarsNumber = oscarsNumber;
+        this.profession = profession;
+    }
+    Professional.prototype.consolePrint = function () {
+        console.log(this);
+    };
+    return Professional;
+}());
+exports.Professional = Professional;
