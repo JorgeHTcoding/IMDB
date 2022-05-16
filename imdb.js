001dd91ab9e48849e0e214481c95f746e6e7e2a8
JorgeHTcoding <jht.coding@gmail.com>
2022-05-16 10:16:51 +0200

    Subida de Clase Imdb y actualización de Main - Jorge

---
+++ imdb.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Imdb = void 0;
+var Imdb = /** @class */ (function () {
+    function Imdb(peliculas) {
+        this.peliculas = peliculas;
+    }
+    return Imdb;
+}());
+exports.Imdb = Imdb;
--- main.js
+++ main.js
@@ -2,13 +2,18 @@
 exports.__esModule = true;
 var professional_1 = require("./professional");
 var movie_1 = require("./movie");
+var imdb_1 = require("./imdb");
 var professional1 = new professional_1.Professional("PepitoPalote", 65, "Male", 71, 189, "Castaño", "Azul", "Caucasico", true, "Irlandés", 58, "Actor");
 var professional2 = new professional_1.Professional("Paquita La del Barrio", 75, "Female", 82, 159, "Rubio", "Avellana", "Caucasico", true, "Mexicana", 0, "Cantante");
 var professional3 = new professional_1.Professional("Clint Eastwood", 91, "Male", 80, 190, "Gris", "Azul", "Caucasico", false, "Estado Unidense", 2, "Actor, Director");
 var professional4 = new professional_1.Professional("Donnie Yen", 58, "Male", 68, 173, "Negro", "Negro", "Asiático", false, "Chino", 0, "Actor, Director, Productor");
-var movie1 = new movie_1.Movie("Dune", 2021, "Estado Unidense", "Ciencia Ficción");
+var movie1 = new movie_1.Movie("Dune", 2021, "Estado Unidense", "Ciencia Ficción : Space Opera");
+var movie2 = new movie_1.Movie("Star Wars: A new Hope", 1977, "Estado Unidense", "Ciencia Ficción : Space Opera");
+var movie3 = new movie_1.Movie("El Quinto Elemento", 1996, "Francesa - Coproducción Estado Unidense", "Ciencia Ficción");
+var imdb1 = new imdb_1.Imdb([movie1, movie2, movie3]);
 // professional1.consolePrint();
 // professional2.consolePrint();
 // professional3.consolePrint();
 // professional4.consolePrint();
-movie1.consolePrintMovie();
+// movie1.consolePrintMovie()
+console.log(imdb1);
